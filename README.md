d6d275958a335074b1d358ad11f2362c1ba747b1
Ephraim Bilson <lite260@gmail.com>
2021-05-15 10:23:35 -0600

    fixing errors in header

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Usage
 
-In *one* file, define `XMLOUT_IMPLEMENTATION` to include the implementation, and subsequently include the header wherever you need it.
+In *one* file before including the xmlout header, define `XMLOUT_IMPLEMENTATION`. This will include the implementation in that file. You may include the file in whatever other segments of your code require it, as long as only one
 
 There are two independent api's available for writing XML files. If you're writing files and don't need to manipulate the file at all before it's written, you can write as you go using the `xml_stream` api. If you want to create the whole document (generally because you want to manipulate it in some way), you can use the `xml_node` api. Example usages for using either follow below.
 
--- xmlout.h
+++ xmlout.h
@@ -3,6 +3,9 @@
 #include <stack>
 #include <map>
 
+#ifndef XMLOUT_H
+#define XMLOUT_H
+
 namespace xmlout
 {
     // private functions
@@ -17,18 +20,18 @@ namespace xmlout
         );
     }
 
-    // ** DEFINITIONS **
+    /* DEFINITIONS */
     class xml_node
     {
     private:
-        std::vector<xml_node*> m_xml_nodes;
+        std::vector<xml_node*> m_children;
 
         std::map<std::string, std::string> m_attributes;
 
         std::string m_tag;
 
     public:
-        xml_node(std::string tag) : m_tag{tag} {}
+        xml_node(std::string tag) : m_tag{tag} {};
 
         xml_node* insert_start(xml_node* xml_node_to_add);
         xml_node* insert_end(xml_node* xml_node_to_add);
@@ -47,26 +50,29 @@ namespace xmlout
     private:
         T m_out_stream;
 
-        std::stack m_tag_stack;
+        std::stack<std::string> m_tag_stack;
 
     public:
-        xml_stream(T stream) : m_out_stream{stream};
+        xml_stream(T stream) : m_out_stream{stream} {};
 
+        xml_stream* write_open(const std::string& m_tag);
         xml_stream* write_open(
-            const std::string &m_tag,
+            const std::string& m_tag,
             std::map<std::string, std::string>& m_attributes
         );
 
         xml_stream* write_close();
+
+        xml_stream* write_empty(const std::string& m_tag);
         xml_stream* write_empty(
-            const std::string &m_tag,
+            const std::string& m_tag,
             std::map<std::string, std::string>& m_attributes
         );
 
-        xml_stream* write_comment();
+        // TODO: xml_stream* write_comment();
     };
 
-    // ** IMPLEMENTATION **
+    /* IMPLEMENTATION */
     #ifdef XMLOUT_IMPLEMENTATION
 
     // private functions
@@ -74,41 +80,43 @@ namespace xmlout
     {
         template <typename T>
         void write_tag(
-            const T &out_stream,
+            const T& out_stream,
             const std::string& tag,
-            std::map<std::string, std::string> &attributes,
+            std::map<std::string, std::string>& attributes,
             bool is_empty)
         {
-            m_out_stream << "<" << m_tag;
-            for (it = attribtues.begin(); it != attribtues.end(); it++)
-                m_out_stream << " \"" << it->first << "\"=\"" << it->second << "\"";
+            out_stream << "<" << tag;
+            std::map<std::string, std::string>::iterator it;
+            for (it = attributes.begin(); it != attributes.end(); it++)
+                out_stream << " \"" << it->first << "\"=\"" << it->second << "\"";
 
-            if (is_empty) m_out_stream << ">";
-            else m_out_stream << "/>";
+            if (is_empty) out_stream << "/>";
+            else out_stream << ">";
         }
     }
 
     xml_node* xml_node::insert_start(xml_node* xml_node_to_add)
     {
-        xml_node.insert(m_xml_nodes.begin(), xml_node_to_add);
+        m_children.insert(m_children.begin(), xml_node_to_add);
         return this;
     }
 
     xml_node* xml_node::insert_end(xml_node* xml_node_to_add)
     {
-        m_xml_nodes.push_back(xml_node);
+        m_children.push_back(xml_node_to_add);
         return this;
     }
     
     xml_node* xml_node::insert(xml_node* xml_node_to_add, int index)
     {
-        xml_node.insert(m_xml_nodes.begin() + index, xml_node_to_add);
+        m_children.insert(m_children.begin() + index, xml_node_to_add);
         return this;
     }
 
     xml_node* xml_node::set(std::string& attribute, std::string& value)
     {
         m_attributes[attribute] = value;
+        return this;
     }
 
     xml_node* xml_node::set_all(std::map<std::string, std::string>& attributes)
@@ -116,43 +124,69 @@ namespace xmlout
         std::map<std::string, std::string>::iterator it;
         for (it = attributes.begin(); it != attributes.end(); it++)
             m_attributes.insert(it->first, it->second);
-    }
 
-    template <typename T>
-    void xml_node::write(const T &m_out_stream)
-    {
-        // write m_tag
-        write_tag(m_out_stream, m_tag, m_attributes, false);
-
-        // write children
-        std::vector<T>::iterator it;
-        for (it = m_xml_nodes.begin(); it != m_xml_nodes.end(); ++it)
-            it->write(m_out_stream);
-
-        // close m_tag
-        m_out_stream << "</" << m_tag << ">";
-    }
-
-    xml_stream* xml_stream::write_open(
-        const std::string& m_tag, std::map<std::string, std::string>& m_attributes)
-    {
-        // write m_tag
-        write_tag(m_out_stream, m_tag, m_attributes, false);
         return this;
     }
 
-    xml_stream* xml_stream::write_close()
+    template <typename T>
+    void xml_node::write(const T& m_out_stream)
+    {
+        // write tag
+        write_tag(m_out_stream, m_tag, m_attributes, false);
+
+        // write children
+        std::vector<xml_node*>::iterator it;
+        for (it = m_children.begin(); it != m_children.end(); ++it)
+            (*it)->write(m_out_stream);
+
+        // close tag
+        m_out_stream << "</" << m_tag << ">";
+    }
+
+    template <class T>
+    xml_stream<T>* xml_stream<T>::write_open(
+        const std::string& tag)
+    {
+        // write tag
+        m_tag_stack.push(tag);
+        m_out_stream << "<" << tag << ">";
+        return this;
+    }
+
+    template <class T>
+    xml_stream<T>* xml_stream<T>::write_open(
+        const std::string& tag, std::map<std::string, std::string>& attributes)
+    {
+        // write tag
+        m_tag_stack.push(tag);
+        write_tag(m_out_stream, tag, attributes, false);
+        return this;
+    }
+
+    template <class T>
+    xml_stream<T>* xml_stream<T>::write_close()
     {
         m_out_stream << "</" << m_tag_stack.pop() << ">";
         return this;
     }
 
-    xml_stream* xml_stream::write_inline(
-        const std::string& m_tag, std::map& attribtues)
+    template <class T>
+    xml_stream<T>* xml_stream<T>::write_empty(const std::string& tag)
     {
-        write_tag(m_out_stream, m_tag, m_attributes, true)
+        // write tag
+        m_out_stream << "<" << tag << "/>";
         return this;
     }
 
-    #endif // XMLOUT_IMPLEMENTATION
+    template <class T>
+    xml_stream<T>* xml_stream<T>::write_empty(
+        const std::string &tag, std::map<std::string, std::string>& attributes)
+    {
+        write_tag(m_out_stream, tag, attributes, true);
+        return this;
+    }
+
+#endif // XMLOUT_IMPLEMENTATION
 }
+
+#endif // XMLOUT_H
